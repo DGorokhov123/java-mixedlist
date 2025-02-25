555f8b5085697dec30b1a773ee5855bd953e2765
Dmitriy Gorokhov <dgorokhov123@gmail.com>
2025-02-25 20:06:07 +0300

    feat: common behaviour imitation performance test

--- test/mixedlist/PerformanceTest.java
+++ test/mixedlist/PerformanceTest.java
@@ -1,17 +1,14 @@
-import mixedlist.MixedList;
+package mixedlist;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        //simpleTest();
-        //iteratorPerfTest();
-        perfTest();
-    }
+public class PerformanceTest {
 
-    private static void simpleTest() {
+    @Test
+    void simpleTest() {
         List<String> strings = new MixedList<>();
-//        List<String> strings = new ArrayList<>();
         strings.add("00");   strings.add("01");   strings.add("02");   strings.add("03");   strings.add("04");
         strings.add("05");   strings.add("06");   strings.add("07");   strings.add("08");   strings.add("09");
         strings.add("10");   strings.add("11");   strings.add("12");   strings.add("13");   strings.add("14");
@@ -27,15 +24,10 @@ public class Main {
         System.out.println();
         while (stringIterator.hasPrevious())  System.out.print(stringIterator.previous() + " ");
         System.out.println();
-        strings.clear();
-        System.out.println(strings);
-        for (String s : strings) System.out.print(s + " ");
-        System.out.println();
     }
 
-
-
-    private static void iteratorPerfTest() {
+    @Test
+    void iteratorPerfTest() {
         long startTime;
         List<String> strings;
         Random random = new Random();
@@ -93,14 +85,12 @@ public class Main {
         System.out.println("-----------------------------------------------------------------------------------------------------------------------");
     }
 
-
-
-
-    public static void perfTest() {
+    @Test
+    void perfTest() {
         long startTime;
         List<String> strings;
         Random random = new Random();
-        int numAddLast = 1000_000;
+        int numAddLast = 10_000_000;
         int numAddFirst = 5_000;
         int numRandomAdd = 1_000;
         int numRandomGet = 1_000;
@@ -178,7 +168,71 @@ public class Main {
         System.out.println("-----------------------------------------------------------------------------------------------------------------------");
     }
 
+    @Test
+    void imitationTest() {
+        long startTime;
+        Random random = new Random();
+        List<String> strings;
+        int numAddLast = 100_000;
+        int numIterations = 10_000;
+        int[][] percent = { { 60, 25, 5, 5, 5 }, { 75, 15, 2, 4, 4 }, { 90, 5, 1, 2, 2 } };
+        for (int i = 0; i < percent.length; i++) {
+            if (Arrays.stream(percent[i]).reduce(0, Integer::sum) != 100) {
+                System.out.println("Sum should be equal 100%");
+                return;
+            }
+        }
+        System.out.printf("%-11s|%5s%-12s |%5s%-12s |%5s%-12s |%n", "", percent[0][0], "% get(i)", percent[1][0], "% get(i)", percent[2][0], "% get(i)");
+        System.out.printf("%-11s|%5s%-12s |%5s%-12s |%5s%-12s |%n", "", percent[0][1], "% add()", percent[1][1], "% add()", percent[2][1], "% add()");
+        System.out.printf("%-11s|%5s%-12s |%5s%-12s |%5s%-12s |%n", "", percent[0][2], "% add(i)", percent[1][2], "% add(i)", percent[2][2], "% add(i)");
+        System.out.printf("%-11s|%5s%-12s |%5s%-12s |%5s%-12s |%n", "", percent[0][3], "% set(i)", percent[1][3], "% set(i)", percent[2][3], "% set(i)");
+        System.out.printf("%-11s|%5s%-12s |%5s%-12s |%5s%-12s |%n", "", percent[0][4], "% delete(i)", percent[1][4], "% delete(i)", percent[2][4], "% delete(i)");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Collection |       Set 1      |       Set 2      |       Set 3      | ");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
 
+        for (int i = 0; i < 3; i++) {
+            if (i == 0) System.out.printf("%-11s|", "ArrayList");
+            //else if (i == 1) continue;
+            else if (i == 1) System.out.printf("%-11s|", "LinkedList");
+            else System.out.printf("%-11s|", "MixedList");
 
+            for (int j = 0; j < percent.length; j++) {
+
+                if (i == 0) strings = new ArrayList<>();
+                else if (i == 1) strings = new LinkedList<>();
+                else strings = new MixedList<>();
+
+                for (int k = 0; k < numAddLast; k++) strings.add("String #" + k);
+
+                startTime = System.currentTimeMillis();
+                //int gets = 0, adds = 0, addis = 0, sets= 0, dels = 0;
+                for (int k = 0; k < numIterations; k++) {
+                    int cmd = random.nextInt(100);
+                    if (cmd < percent[j][0]) {       // get(i)
+                        strings.get(random.nextInt(strings.size()));
+                        //gets++;
+                    } else if (cmd < percent[j][0] + percent[j][1]) {     // add()
+                        strings.add("Additional String");
+                        //adds++;
+                    } else if (cmd < percent[j][0] + percent[j][1] + percent[j][2]) {        // add(i)
+                        strings.add(random.nextInt(strings.size()), "Additional string");
+                        //addis++;
+                    } else if (cmd < percent[j][0] + percent[j][1] + percent[j][2] + percent[j][3]) {        // set(i)
+                        strings.set(random.nextInt(strings.size()), "Some string to set");
+                        //sets++;
+                    } else {           // delete(i)
+                        strings.remove(random.nextInt(strings.size()));
+                        //dels++;
+                    }
+                }
+                System.out.printf("%17s |", (System.currentTimeMillis() - startTime) + " ms");
+                //System.out.printf(" %d, %d, %d, %d, %d ", gets*100/numIterations, adds*100/numIterations,
+                //        addis*100/numIterations, sets*100/numIterations, dels*100/numIterations);
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
+    }
 
 }
